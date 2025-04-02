34dd8fae66c07682094fd2bc0f933b29b7b93ec1
gabydd <gabydinnerdavid@gmail.com>
2025-04-01 21:22:23 -0400

    allow 0.15

--- build.zig
+++ build.zig
@@ -2,21 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 const assert = std.debug.assert;
 
-const zig_version = std.SemanticVersion{ .major = 0, .minor = 14, .patch = 0 };
-
-comptime {
-    // Compare versions while allowing different pre/patch metadata.
-    const zig_version_eq = zig_version.major == builtin.zig_version.major and
-        zig_version.minor == builtin.zig_version.minor and
-        zig_version.patch == builtin.zig_version.patch;
-    if (!zig_version_eq) {
-        @compileError(std.fmt.comptimePrint(
-            "unsupported zig version: expected {}, found {}",
-            .{ zig_version, builtin.zig_version },
-        ));
-    }
-}
-
 const Example = enum {
     none,
     all,
